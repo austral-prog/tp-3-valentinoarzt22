1db3e79ef19eb89d228eb5e30bbc889c843eb59b
valentinoarzt22 <vrarzt@mail.austral.edu.ar>
2025-04-07 11:02:47 -0300

    Update slice_advanced.py

--- slice_advanced.py
+++ slice_advanced.py
@@ -1,4 +1,6 @@
 def slice_advanced():
+    texto = input('Ingresar texto:' )
+    print(texto[4::2])
     # Código a implementar utilizando input.
 
 # Para verificar este ejercicio ejecutar el comando
