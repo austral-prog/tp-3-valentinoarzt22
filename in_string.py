4d35d77d154d51879ddf74c8acbc8fed9e3918d4
valentinoarzt22 <vrarzt@mail.austral.edu.ar>
2025-04-07 10:28:41 -0300

    Update in_string.py

--- in_string.py
+++ in_string.py
@@ -2,23 +2,23 @@ def check_vowels():
     # Código a implementar utilizando input.
     nombre = input('Ingresar nombre:' )
     if nombre.find('a') != -1:
-        print('contiene a: True')
+        print('Contiene a: True')
     else:
         print('Contiene a: False')
     if nombre.find('e') != -1:
-        print('contiene e: True')
+        print('Contiene e: True')
     else:
         print('Contiene e: False')
     if nombre.find('i') != -1:
-        print('contiene i: True')
+        print('Contiene i: True')
     else:
         print('Contiene i: False')
     if nombre.find('o') != -1:
-        print('contiene o: True')
+        print('Contiene o: True')
     else:
         print('Contiene o: False')
     if nombre.find('u') != -1:
-        print('contiene u: True')
+        print('Contiene u: True')
     else:
         print('Contiene u: False')
 # Para verificar este ejercicio ejecutar el comando
